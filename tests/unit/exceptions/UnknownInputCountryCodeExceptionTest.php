5f47bd9bc88de5f47f364179f84c63929b604b29
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-15 01:32:07 +0400

    [CVFV-64] renamed.

--- tests/unit/exceptions/UnknownInputCountryCodeExceptionTest.php
+++ tests/unit/exceptions/UnknownInputCountryCodeExceptionTest.php
@@ -3,21 +3,21 @@
 namespace rocketfellows\CountryVatNumberFormatValidator\tests\unit\exceptions;
 
 use rocketfellows\CountryVatNumberFormatValidator\exceptions\InputCountryCodeException;
-use rocketfellows\CountryVatNumberFormatValidator\exceptions\UnknownCountryCodeException;
+use rocketfellows\CountryVatNumberFormatValidator\exceptions\UnknownInputCountryCodeException;
 use Throwable;
 
 /**
  * @group country-vat-number-format-validator-exceptions
  */
-class UnknownCountryCodeExceptionTest extends InputCountryCodeExceptionTest
+class UnknownInputCountryCodeExceptionTest extends InputCountryCodeExceptionTest
 {
     protected function getExceptionWithFullParameters(string $countryCode, string $message, int $code, ?Throwable $previousException): InputCountryCodeException
     {
-        return new UnknownCountryCodeException($countryCode, $message, $code, $previousException);
+        return new UnknownInputCountryCodeException($countryCode, $message, $code, $previousException);
     }
 
     protected function getExceptionWithOnlyInputCountryCodeParameter(string $countryCode): InputCountryCodeException
     {
-        return new UnknownCountryCodeException($countryCode);
+        return new UnknownInputCountryCodeException($countryCode);
     }
 }
