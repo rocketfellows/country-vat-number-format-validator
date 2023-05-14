e1bf09d78d91e4bb1da7c494fec0febbfd117d68
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-14 17:41:59 +0400

    [CVFV-64] covered.

--- tests/unit/VatNumberFormatValidationResultTest.php
+++ tests/unit/VatNumberFormatValidationResultTest.php
@@ -6,6 +6,25 @@ use PHPUnit\Framework\TestCase;
 
 class VatNumberFormatValidationResultTest extends TestCase
 {
+    /**
+     * @dataProvider getVatNumberFormatValidationResultProvidedData
+     */
+    public function testInitVatNumberFormatValidationResultFromInputParameters(
+        bool $isValid,
+        array $passedValidatorsClasses,
+        ?string $successfullyValidatorClass
+    ): void {
+        $vatNumberFormatValidationResult = new VatNumberFormatValidationResult(
+            $isValid,
+            $passedValidatorsClasses,
+            $successfullyValidatorClass
+        );
+
+        $this->assertEquals($isValid, $vatNumberFormatValidationResult->isValid());
+        $this->assertEquals($passedValidatorsClasses, $vatNumberFormatValidationResult->getPassedValidatorsClasses());
+        $this->assertEquals($successfullyValidatorClass, $vatNumberFormatValidationResult->getSuccessfullyValidatorClass());
+    }
+
     public function getVatNumberFormatValidationResultProvidedData(): array
     {
         return [
