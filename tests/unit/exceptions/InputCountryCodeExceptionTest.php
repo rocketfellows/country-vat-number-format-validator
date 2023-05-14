62a192c81e5104934eab52d152f7241ece50f17a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-14 18:50:51 +0400

    [CVFV-64] covered input country code exception with full parameters.

--- tests/unit/exceptions/InputCountryCodeExceptionTest.php
+++ tests/unit/exceptions/InputCountryCodeExceptionTest.php
@@ -8,6 +8,70 @@ use Throwable;
 class InputCountryCodeExceptionTest extends TestCase
 {
     /**
+     * @dataProvider getInputCountryCodeExceptionProvidedData
+     */
+    public function testInputCountryCodeExceptionWithFullParameters(
+        string $countryCode,
+        string $message,
+        int $code,
+        ?Throwable $previousException
+    ): void {
+        /** @var Throwable $exception */
+        $exception = $this->getMockForAbstractClass(
+            InputCountryCodeException::class,
+            [
+                $countryCode,
+                $message,
+                $code,
+                $previousException
+            ]
+        );
+
+        $this->assertEquals($countryCode, $exception->getInputCountryCode());
+        $this->assertEquals($message, $exception->getMessage());
+        $this->assertEquals($code, $exception->getCode());
+        $this->assertEquals($previousException, $exception->getPrevious());
+    }
+
+    public function getInputCountryCodeExceptionProvidedData(): array
+    {
+        $previousException = $this->createMock(Throwable::class);
+
+        return [
+            'message set, code set, previous exception set' => [
+                'countryCode' => 'foo',
+                'message' => 'foo',
+                'code' => 1,
+                'previousException' => $previousException,
+            ],
+            'message empty, code set, previous exception set' => [
+                'countryCode' => 'foo',
+                'message' => '',
+                'code' => 1,
+                'previousException' => $previousException,
+            ],
+            'message set, code set, previous exception not set' => [
+                'countryCode' => 'foo',
+                'message' => 'foo',
+                'code' => 1,
+                'previousException' => null,
+            ],
+            'message empty, code set, previous exception not set' => [
+                'countryCode' => 'foo',
+                'message' => '',
+                'code' => 1,
+                'previousException' => null,
+            ],
+            'message empty, code set default, previous exception set' => [
+                'countryCode' => 'foo',
+                'message' => '',
+                'code' => 0,
+                'previousException' => null,
+            ],
+        ];
+    }
+
+    /**
      * @dataProvider getExceptionWithInputCountryCodeProvidedData
      */
     public function testInitExceptionWithOnlyInputCountryCode(string $countryCode): void
