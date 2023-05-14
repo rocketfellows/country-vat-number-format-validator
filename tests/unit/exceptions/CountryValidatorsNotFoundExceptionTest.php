899d823f86d11a250994fc1aadcb68b404debfc3
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-14 20:08:22 +0400

    [CVFV-64] covered country validators not found exception.

---
+++ tests/unit/exceptions/CountryValidatorsNotFoundExceptionTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace rocketfellows\CountryVatNumberFormatValidator\tests\unit\exceptions;
+
+use rocketfellows\CountryVatNumberFormatValidator\exceptions\InputCountryCodeException;
+use Throwable;
+
+/**
+ * @group country-vat-number-format-validator-exceptions
+ */
+class CountryValidatorsNotFoundExceptionTest extends InputCountryCodeExceptionTest
+{
+    protected function getExceptionWithFullParameters(
+        string $countryCode,
+        string $message,
+        int $code,
+        ?Throwable $previousException
+    ): InputCountryCodeException {
+        return new CountryValidatorsNotFoundException(
+            $countryCode,
+            $message,
+            $code,
+            $previousException
+        );
+    }
+
+    protected function getExceptionWithOnlyInputCountryCodeParameter(string $countryCode): InputCountryCodeException
+    {
+        return new CountryValidatorsNotFoundException($countryCode);
+    }
+}
