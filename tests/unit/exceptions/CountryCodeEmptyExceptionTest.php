a713cd4302a7b906fdfd8e1a1df5366b25ced96b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-14 19:10:40 +0400

    [CVFV-64] covered country code empty exception.

---
+++ tests/unit/exceptions/CountryCodeEmptyExceptionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace rocketfellows\CountryVatNumberFormatValidator\tests\unit\exceptions;
+
+use rocketfellows\CountryVatNumberFormatValidator\exceptions\InputCountryCodeException;
+use Throwable;
+
+class CountryCodeEmptyExceptionTest extends InputCountryCodeExceptionTest
+{
+    protected function getExceptionWithFullParameters(string $countryCode, string $message, int $code, ?Throwable $previousException): InputCountryCodeException
+    {
+        return new CountryCodeEmptyException($countryCode, $message, $code, $previousException);
+    }
+
+    protected function getExceptionWithOnlyInputCountryCodeParameter(string $countryCode): InputCountryCodeException
+    {
+        return new CountryCodeEmptyException($countryCode);
+    }
+}
