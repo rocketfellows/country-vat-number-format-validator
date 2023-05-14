8c39181b9a65ad1c46989ea33d5b21dacc772f5f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-15 01:24:09 +0400

    [CVFV-64] implemented country code empty validation.

--- src/VatNumberFormatValidatorService.php
+++ src/VatNumberFormatValidatorService.php
@@ -2,6 +2,25 @@
 
 namespace rocketfellows\CountryVatNumberFormatValidator;
 
+use rocketfellows\CountryVatNumberFormatValidator\exceptions\CountryCodeEmptyException;
+
 class VatNumberFormatValidatorService
 {
+    /**
+     * @throws CountryCodeEmptyException
+     */
+    public function validateCountryVatNumber(string $countryCode, string $vatNumber): void
+    {
+        $this->validateInputCountryCode($countryCode);
+    }
+
+    /**
+     * @throws CountryCodeEmptyException
+     */
+    private function validateInputCountryCode(string $countryCode): void
+    {
+        if (empty($countryCode)) {
+            throw new CountryCodeEmptyException($countryCode);
+        }
+    }
 }
