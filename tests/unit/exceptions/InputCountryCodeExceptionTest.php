77ed4b4805e00c26fa9545c20a163800476ac4b8
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-14 18:40:45 +0400

    [CVFV-64] setup test.

---
+++ tests/unit/exceptions/InputCountryCodeExceptionTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace rocketfellows\CountryVatNumberFormatValidator\tests\unit\exceptions;
+
+use PHPUnit\Framework\TestCase;
+
+class InputCountryCodeExceptionTest extends TestCase
+{
+}
