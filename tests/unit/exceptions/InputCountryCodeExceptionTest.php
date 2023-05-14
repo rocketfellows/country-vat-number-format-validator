c1cab6b0b2972c55a336da1f9db3e2e9c4ade5c2
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-14 18:45:42 +0400

    [CVFV-64] covered init exception with only input country code.

--- tests/unit/exceptions/InputCountryCodeExceptionTest.php
+++ tests/unit/exceptions/InputCountryCodeExceptionTest.php
@@ -3,7 +3,41 @@
 namespace rocketfellows\CountryVatNumberFormatValidator\tests\unit\exceptions;
 
 use PHPUnit\Framework\TestCase;
+use Throwable;
 
 class InputCountryCodeExceptionTest extends TestCase
 {
+    /**
+     * @dataProvider getExceptionWithInputCountryCodeProvidedData
+     */
+    public function testInitExceptionWithOnlyInputCountryCode(string $countryCode): void
+    {
+        /** @var Throwable $exception */
+        $exception = $this->getMockForAbstractClass(
+            InputCountryCodeException::class,
+            [
+                $countryCode,
+            ]
+        );
+
+        $this->assertEquals($countryCode, $exception->getInputCountryCode());
+        $this->assertEmpty($exception->getMessage());
+        $this->assertEquals(0, $exception->getCode());
+        $this->assertNull($exception->getPrevious());
+    }
+
+    public function getExceptionWithInputCountryCodeProvidedData(): array
+    {
+        return [
+            [
+                'countryCode' => 'foo',
+            ],
+            [
+                'countryCode' => 'fooBar',
+            ],
+            [
+                'countryCode' => '',
+            ],
+        ];
+    }
 }
