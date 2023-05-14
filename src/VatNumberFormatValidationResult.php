854f2e6236a38901896ad06abdb49b8e146a2875
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-14 17:53:07 +0400

    [CVFV-64] added attributes.

--- src/VatNumberFormatValidationResult.php
+++ src/VatNumberFormatValidationResult.php
@@ -4,4 +4,7 @@ namespace rocketfellows\CountryVatNumberFormatValidator;
 
 class VatNumberFormatValidationResult
 {
+    private $isValid;
+    private $passedValidatorsClasses;
+    private $successfullyValidatorClass;
 }
