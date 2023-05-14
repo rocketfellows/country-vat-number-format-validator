75910becf669e998d4564043c7b33a6d6e5c6cae
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-05-14 17:54:34 +0400

    [CVFV-64] setup constructor.

--- src/VatNumberFormatValidationResult.php
+++ src/VatNumberFormatValidationResult.php
@@ -7,4 +7,14 @@ class VatNumberFormatValidationResult
     private $isValid;
     private $passedValidatorsClasses;
     private $successfullyValidatorClass;
+
+    public function __construct(
+        bool $isValid,
+        array $passedValidatorsClasses,
+        ?string $successfullyValidatorClass = null
+    ) {
+        $this->isValid = $isValid;
+        $this->passedValidatorsClasses = $passedValidatorsClasses;
+        $this->successfullyValidatorClass = $successfullyValidatorClass;
+    }
 }
