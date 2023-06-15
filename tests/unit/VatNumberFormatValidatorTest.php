f6caae3d5c701aa1b7e4a0982a897317e19cbf9e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-06-15 21:10:39 +0400

    [CVFV-64] setup test.

--- tests/unit/VatNumberFormatValidatorTest.php
+++ tests/unit/VatNumberFormatValidatorTest.php
@@ -2,11 +2,30 @@
 
 namespace rocketfellows\CountryVatNumberFormatValidator\tests\unit;
 
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidator;
+use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidatorService;
 
 /**
  * @group country-vat-number-format-validator-services
  */
 class VatNumberFormatValidatorTest extends TestCase
 {
+    /**
+     * @var VatNumberFormatValidator
+     */
+    private $vatNumberFormatValidator;
+
+    /**
+     * @var VatNumberFormatValidatorService|MockObject
+     */
+    private $vatNumberFormatValidatorService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->vatNumberFormatValidatorService = $this->createMock(VatNumberFormatValidatorService::class);
+    }
 }
