70bbac81b9c2ffda12c1b3a09378b775609cc553
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-07-08 19:10:53 +0400

    [CVFV-64] covered handling validation exceptions.

--- tests/unit/VatNumberFormatValidatorTest.php
+++ tests/unit/VatNumberFormatValidatorTest.php
@@ -4,9 +4,14 @@ namespace rocketfellows\CountryVatNumberFormatValidator\tests\unit;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use rocketfellows\CountryVatNumberFormatValidator\exceptions\CountryCodeEmptyException;
+use rocketfellows\CountryVatNumberFormatValidator\exceptions\CountryValidatorsNotFoundException;
+use rocketfellows\CountryVatNumberFormatValidator\exceptions\UnknownInputCountryCodeException;
+use rocketfellows\CountryVatNumberFormatValidator\exceptions\VatNumberValidatingException;
 use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidationResult;
 use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidator;
 use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidatorService;
+use Throwable;
 
 /**
  * @group country-vat-number-format-validator-services
@@ -77,6 +82,50 @@ class VatNumberFormatValidatorTest extends TestCase
                 self::VAT_NUMBER_TEST_VALUE
             )
         );
+    }
+
+    /**
+     * @dataProvider getHandlingValidationExceptionsProvidedData
+     */
+    public function testHandlingValidationExceptions(Throwable $thrownException, string $expectedExceptionClass): void
+    {
+        $this->vatNumberFormatValidatorService
+            ->expects($this->once())
+            ->method('validateCountryVatNumber')
+            ->willThrowException($thrownException);
+
+        $this->expectException($expectedExceptionClass);
+
+        $this->vatNumberFormatValidator->isValid(
+            self::COUNTRY_CODE_TEST_VALUE,
+            self::VAT_NUMBER_TEST_VALUE
+        );
+    }
+
+    public function getHandlingValidationExceptionsProvidedData(): array
+    {
+        return [
+            'thrown CountryCodeEmptyException' => [
+                'thrownException' => $this->createMock(CountryCodeEmptyException::class),
+                'expectedExceptionClass' => CountryCodeEmptyException::class,
+            ],
+            'thrown CountryValidatorsNotFoundException' => [
+                'thrownException' => $this->createMock(CountryValidatorsNotFoundException::class),
+                'expectedExceptionClass' => CountryValidatorsNotFoundException::class,
+            ],
+            'thrown UnknownInputCountryCodeException' => [
+                'thrownException' => $this->createMock(UnknownInputCountryCodeException::class),
+                'expectedExceptionClass' => UnknownInputCountryCodeException::class,
+            ],
+            'thrown VatNumberValidatingException' => [
+                'thrownException' => $this->createMock(VatNumberValidatingException::class),
+                'expectedExceptionClass' => VatNumberValidatingException::class,
+            ],
+            'thrown Throwable' => [
+                'thrownException' => $this->createMock(Throwable::class),
+                'expectedExceptionClass' => Throwable::class,
+            ],
+        ];
     }
 
     private function getVatNumberFormatValidationResultMock(): MockObject
