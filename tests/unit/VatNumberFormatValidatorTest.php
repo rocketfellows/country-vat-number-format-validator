18cf7e3a36dcb385b9abbe518c2fc8692a62e22c
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-07-08 17:35:50 +0400

    [CVFV-64] covered country vat number is valid.

--- tests/unit/VatNumberFormatValidatorTest.php
+++ tests/unit/VatNumberFormatValidatorTest.php
@@ -4,6 +4,7 @@ namespace rocketfellows\CountryVatNumberFormatValidator\tests\unit;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidationResult;
 use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidator;
 use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidatorService;
 
@@ -12,6 +13,9 @@ use rocketfellows\CountryVatNumberFormatValidator\VatNumberFormatValidatorServic
  */
 class VatNumberFormatValidatorTest extends TestCase
 {
+    private const COUNTRY_CODE_TEST_VALUE = 'foo';
+    private const VAT_NUMBER_TEST_VALUE = '123';
+
     /**
      * @var VatNumberFormatValidator
      */
@@ -27,5 +31,38 @@ class VatNumberFormatValidatorTest extends TestCase
         parent::setUp();
 
         $this->vatNumberFormatValidatorService = $this->createMock(VatNumberFormatValidatorService::class);
+
+        $this->vatNumberFormatValidator = new VatNumberFormatValidator(
+            $this->vatNumberFormatValidatorService
+        );
+    }
+
+    public function testCountryVatNumberIsValid(): void
+    {
+        $validationResult = $this->getVatNumberFormatValidationResultMock();
+        $validationResult
+            ->expects($this->once())
+            ->method('isValid')
+            ->willReturn(true);
+
+        $this->vatNumberFormatValidatorService
+            ->expects($this->once())
+            ->method('validateCountryVatNumber')
+            ->willReturn($validationResult);
+
+        $this->assertTrue(
+            $this->vatNumberFormatValidator->isValid(
+                self::COUNTRY_CODE_TEST_VALUE,
+                self::VAT_NUMBER_TEST_VALUE
+            )
+        );
+    }
+
+    private function getVatNumberFormatValidationResultMock(): MockObject
+    {
+        /** @var VatNumberFormatValidationResult|MockObject $mock */
+        $mock = $this->createMock(VatNumberFormatValidationResult::class);
+
+        return $mock;
     }
 }
